b087dd5eadf1549ce0ed5d0e392f0f2259581dd2
monishs777 <monishdhanalakshmi@gmail.com>
2026-07-30 15:34:59 +0530

    Refactor dice roll logic for cleaner code

--- ludo.js
+++ ludo.js
@@ -1,30 +1,30 @@
 var randomNumber1 = Math.floor(Math.random() * 6) + 1;
 if(randomNumber1===1){
     document.querySelectorAll("img")[0].setAttribute("src","image/dice1.png");
-    document.querySelector("p")[1].innerHTML="The Number is 1 "
+    document.querySelector("p").innerHTML="The Number is 1 "
 }
 else if(randomNumber1===2){
     document.querySelectorAll("img")[0].setAttribute("src","image/dice2.png");
-    document.querySelector("p")[1].innerHTML="The Number is 2 "
+    document.querySelector("p").innerHTML="The Number is 2 "
 
 }
 else if(randomNumber1===3){
     document.querySelectorAll("img")[0].setAttribute("src","image/dice3.png");
-    document.querySelector("p")[1].innerHTML="The Number is 3 "
+    document.querySelector("p").innerHTML="The Number is 3 "
 
 }
 else if(randomNumber1===4){
     document.querySelectorAll("img")[0].setAttribute("src","image/dice4.png");
-        document.querySelector("p")[1].innerHTML="The Number is 4 "
+        document.querySelector("p").innerHTML="The Number is 4 "
 
 }
 else if(randomNumber1===5){
     document.querySelectorAll("img")[0].setAttribute("src","image/dice5.png");
-        document.querySelector("p")[1].innerHTML="The Number is 5 "
+        document.querySelector("p").innerHTML="The Number is 5 "
 
 }
 else {
     document.querySelectorAll("img")[0].setAttribute("src","image/dice6.png");
-        document.querySelector("p")[1].innerHTML="The Number is 6 "
+        document.querySelector("p").innerHTML="The Number is 6 "
 
 }
