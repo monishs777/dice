3144ae6b9dbc26d769da241a09c16057780afb6e
monishs777 <monishdhanalakshmi@gmail.com>
2026-09-18 06:28:49 +0530

    Update ludo.js

--- ludo.js
+++ ludo.js
@@ -1,30 +1,13 @@
-var randomNumber1 = Math.floor(Math.random() * 6) + 1;
-if(randomNumber1===1){
-    document.querySelectorAll("img")[0].setAttribute("src","image/dice1.png");
-    document.querySelector("p").innerHTML="The Number is 1 "
-}
-else if(randomNumber1===2){
-    document.querySelectorAll("img")[0].setAttribute("src","image/dice2.png");
-    document.querySelector("p").innerHTML="The Number is 2 "
+var diceImage = document.querySelector("#diceImage");
+var diceNumber = document.querySelector("#diceNumber");
 
-}
-else if(randomNumber1===3){
-    document.querySelectorAll("img")[0].setAttribute("src","image/dice3.png");
-    document.querySelector("p").innerHTML="The Number is 3 "
+function rollDice() {
 
-}
-else if(randomNumber1===4){
-    document.querySelectorAll("img")[0].setAttribute("src","image/dice4.png");
-        document.querySelector("p").innerHTML="The Number is 4 "
+    var randomNumber = Math.floor(Math.random() * 6) + 1;
 
-}
-else if(randomNumber1===5){
-    document.querySelectorAll("img")[0].setAttribute("src","image/dice5.png");
-        document.querySelector("p").innerHTML="The Number is 5 "
+    diceImage.setAttribute("src", "image/dice" + randomNumber + ".png");
 
+    diceNumber.innerHTML = "The Number is " + randomNumber;
 }
-else {
-    document.querySelectorAll("img")[0].setAttribute("src","image/dice6.png");
-        document.querySelector("p").innerHTML="The Number is 6 "
 
-}
+diceImage.addEventListener("click", rollDice);
